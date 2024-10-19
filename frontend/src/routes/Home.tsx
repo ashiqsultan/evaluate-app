4fe561f22576cf59366111cd0ecb60aa54ce92e1
ashiqsultan <ashiqdrive@gmail.com>
2024-10-19 17:57:27 +0530

    Added Shadcn

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,10 +1,12 @@
 import { useItemTypeById } from '../store/queries';
+import { Button } from '../components/ui/button';
 const Home = () => {
   const { data, isLoading } = useItemTypeById('id');
   return (
     <>
       <h1>This is Home Page</h1>
       <p>{isLoading ? 'Loading...' : data}</p>
+      <Button>Click me</Button>
     </>
   );
 };
